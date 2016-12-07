a2a6a440cdd3111f31245fb14b9c780e83656eb5
majortom327 <vthomas@student.42.fr>
2016-12-07 02:40:12 +0100

    Leaks cmd fix

--- src/input/parse/clean.c
+++ src/input/parse/clean.c
@@ -6,17 +6,31 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 02:47:34 by vthomas           #+#    #+#             */
-/*   Updated: 2016/12/07 01:24:51 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/12/07 02:35:07 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <libft.h>
 
+static void	sf_cleantab(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\t')
+			str[i] = ' ';
+		i++;
+	}
+}
+
 char	**clean_cmd(char **cmd)
 {
 	char	**command;
 
+	sf_cleantab(*cmd);
 	command = ft_strsplit(*cmd, ' ');
 	dbg_var_array_str("clean_cmd", "command", (const char **)command, 3);
 	ft_strdel(cmd);
--- src/other/loop.c
+++ src/other/loop.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 04:50:47 by vthomas           #+#    #+#             */
-/*   Updated: 2016/12/07 01:42:12 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/12/07 02:39:12 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 #include <libft.h>
 #include <get_next_line.h>
 #include <stdlib.h>
+
+static void	sf_deletecmd(char **cmd)
+{
+	int i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		ft_strdel(&cmd[i]);
+		i++;
+	}
+	ft_memdel((void **)&cmd);
+}
 
 void	loop(t_sh *sh)
 {
@@ -26,23 +39,21 @@ void	loop(t_sh *sh)
 	if (get_prompt(sh))
 		exit(EXIT_FAILURE);
 	ft_putstr(sh->prompt);
-	cmd = NULL;
 	while ((ret = get_next_line(0, &buf)))
 	{
 		dbg_var_str("loop", "buf", buf, 1);
-		//ft_memdel(&(cmd));
 		cmd = NULL;
 		l = ft_strlen(buf);
 		if (l != 0)
 			cmd = clean_cmd(&buf);
 		if (l == 0 || cmd == NULL)
 		{
-			dbg_info("loop", "cmd == NULL", 2);
 			ft_putstr(sh->prompt);
 			ft_strdel(&buf);
 			continue ;
 		}
 		dbg_var_array_str("loop", "cmd", cmd, 2);
+		sf_deletecmd(cmd);
 //		execute(sh, cmd);
 		ft_strdel(&buf);
 		if (get_prompt(sh))
