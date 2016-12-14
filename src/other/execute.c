85662e11ff139327feda953ae7b53f0dc9f37834
majortom327 <vthomas@student.42.fr>
2016-12-14 05:00:04 +0100

    Add executable permission

--- src/other/execute.c
+++ src/other/execute.c
@@ -6,18 +6,29 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 02:40:41 by vthomas           #+#    #+#             */
-/*   Updated: 2016/12/14 04:21:53 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/12/14 04:59:25 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <libft.h>
+#include <unistd.h>
 
 int	execute_exe(t_sh *sh, char **cmd)
 {
+	if (access(cmd[0], X_OK) == -1 || access(cmd[0], F_OK) == -1)
+	{
+		ft_putstr(sh->progname);
+		if (access(cmd[0], F_OK) == -1)
+			ft_putstr(": no such file or directory: ");
+		else
+			ft_putstr(": permission denied: ");
+		ft_putendl(cmd[0]);
+		return (1);
+	}
 	ft_putendl("EXTERNAL PROGRAM");
 	ft_putendl(cmd[0]);
-	return ((sh->ret = 0));
+	return (0);
 }
 
 int	execute(t_sh *sh, char **cmd)
@@ -33,6 +44,8 @@ int	execute(t_sh *sh, char **cmd)
 		return (sh->ret = 0);
 	}
 	if (!prog_search(sh, cmd))
+		return (sh->ret = execute_exe(sh, cmd));
+	else if (access(cmd[0], F_OK) != -1 || access(cmd[0], F_OK) == -1)
 		return (sh->ret = execute_exe(sh, cmd));
 	ft_putstr(sh->progname);
 	ft_putstr(": command not found: ");
